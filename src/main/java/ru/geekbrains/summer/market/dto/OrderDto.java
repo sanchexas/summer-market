f55026195838ea0a9c38be20a259b8fa137f4dc8
sergey-sanches-peres <sanchesgithub@mail.ru>
2021-08-23 16:48:39 +0300

    update

--- src/main/java/ru/geekbrains/summer/market/dto/OrderDto.java
+++ src/main/java/ru/geekbrains/summer/market/dto/OrderDto.java
@@ -6,6 +6,8 @@ import ru.geekbrains.summer.market.model.Order;
 import ru.geekbrains.summer.market.model.Product;
 
 import java.math.BigDecimal;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @NoArgsConstructor
 @Data
@@ -14,11 +16,13 @@ public class OrderDto {
     private String address;
     private String phone;
     private BigDecimal price;
+    private List<OrderItemDto> items;
 
     public OrderDto(Order order) {
         this.id = order.getId();
         this.address = order.getAddress();
         this.phone = order.getPhone();
         this.price = order.getPrice();
+        this.items = order.getItems().stream().map(OrderItemDto::new).collect(Collectors.toList());
     }
 }
